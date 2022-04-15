bde6458a7d2703ec4757d3e615f53ccb18faa738
0xmycf <mycf.mycf.mycf@gmail.com>
2022-04-15 15:35:33 +0200

    minor fixes

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,10 +1,6 @@
 use futures::executor::block_on;
 
 fn main() {
-    block_on(main::run()).iter().for_each(printme);
-}
-
-fn printme<A: std::fmt::Display>(printable: A) {
-    println!("{printable}");
+    block_on(main::run()).iter().for_each(|x| println!("{}", x));
 }
 
